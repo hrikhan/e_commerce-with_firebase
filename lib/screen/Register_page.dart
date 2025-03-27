1419fcaf222c89ed182129b6123bf285393f9c2e
hridoykhan51 <hridoykhan6786@gmail.com>
2025-03-28 03:06:01 +0600

    login founctinality complete

--- lib/screen/Register_page.dart
+++ lib/screen/Register_page.dart
@@ -1,3 +1,5 @@
+import 'package:ecommerce_with_firebase/buttom_navbar/bottomnavbar.dart';
+import 'package:ecommerce_with_firebase/controllars/auth_controllars.dart';
 import 'package:ecommerce_with_firebase/screen/Profile_setup_page.dart';
 import 'package:ecommerce_with_firebase/screen/Welcome_screen.dart';
 import 'package:flutter/material.dart';
@@ -10,121 +12,149 @@ import '../widgets/Custom_textfeild.dart';
 import '../widgets/custom_button.dart';
 
 class RegisterPage extends StatelessWidget {
-  const RegisterPage({super.key});
-
+  RegisterPage({super.key});
+  AuthControllars authControllars = Get.put(AuthControllars());
+  final TextEditingController _emailcontroller = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return
-        Scaffold(
-          resizeToAvoidBottomInset: false,
-          body: Padding(
-            padding: const EdgeInsets.only(top: 40, left: 10, right: 10),
-            child: Container(
-              width: allsize.fullwidth(context),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  InkWell(
-                    onTap:(){ Get.to(WelcomeScreen());},
-                    child: Container(
-                      height: 80,
-                      width: 80,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: allcolors.secondarycolor,
+    return GetBuilder<AuthControllars>(builder: (_) {
+      print('error');
+      return Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Padding(
+          padding: const EdgeInsets.only(top: 40, left: 10, right: 10),
+          child: Container(
+            width: allsize.fullwidth(context),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                InkWell(
+                  onTap: () {
+                    Get.to(WelcomeScreen());
+                  },
+                  child: Container(
+                    height: 80,
+                    width: 80,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: allcolors.secondarycolor,
+                    ),
+                    child: Center(
+                        child: Icon(
+                      Icons.arrow_back,
+                      color: Colors.black,
+                      size: 40,
+                    )),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Center(
+                  child: Column(
+                    children: [
+                      Text(
+                        'Create Account',
+                        style: allstyles.headingstyle,
                       ),
-                      child: Center(
-                          child: Icon(
-                            Icons.arrow_back,
-                            color: Colors.black,
-                            size: 40,
-                          )),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Center(
-                    child: Column(
-                      children: [
-                        Text(
-                          'Create Account',
-                          style: allstyles.headingstyle,
-                        ),
-                        SizedBox(
-                          height: 30,
-                        ),
-                        Text(
-                          'Create an account so you can explore all the ',
-                          style: allstyles.subtitlestyle.copyWith(fontSize: 17),
-                        ),
-                        Text(
-                          'existing job ',
-                          style: allstyles.subtitlestyle.copyWith(fontSize: 17),
-                        ),
-
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(6.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        CustomTextfeild(
-                          hintText: ' Email',
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        CustomTextfeild(
-                          hintText: ' password',
-                        ),
-
-                        SizedBox(
-                          height: 20,
-                        ),
-                        CustomTextfeild(
-                          hintText: ' Confirmed password',
-                        ),
-
-                        SizedBox(height: 30),
-                        Custombutton(
-                            height: 50,
-                            width: allsize.fullwidth(context),
-                            ontap: () {
-                              Get.to(ProfileSetupPage());
-
-                            },
-                            size: allsize.large,
-                            text: "Register",
-                            color: allcolors.primarycolor,
-                            Textcolor: allcolors.white),
-
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height:25,
-                  ),
-                  Center(
-                    child: InkWell(
-                      onTap: (){},
-                      child: Text(
-                        'Already have an account',
-                        textAlign: TextAlign.end,
-                        style: allstyles.subtitlestyle,
+                      SizedBox(
+                        height: 30,
                       ),
+                      Text(
+                        'Create an account so you can explore all the ',
+                        style: allstyles.subtitlestyle.copyWith(fontSize: 17),
+                      ),
+                      Text(
+                        'existing job ',
+                        style: allstyles.subtitlestyle.copyWith(fontSize: 17),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(6.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      CustomTextfeild(
+                        controller: _emailcontroller,
+                        hintText: ' Email',
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      CustomTextfeild(
+                        controller: _passwordController,
+                        hintText: ' password',
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      CustomTextfeild(
+                        controller: _confirmController,
+                        hintText: ' Confirmed password',
+                      ),
+                      SizedBox(height: 30),
+                      authControllars.setloading
+                          ? Center(
+                              child: CircularProgressIndicator(
+                                color: allcolors.primarycolor,
+                              ),
+                            )
+                          : Custombutton(
+                              height: 50,
+                              width: allsize.fullwidth(context),
+                              ontap: () {
+                                if (_emailcontroller.text.isNotEmpty &&
+                                    _passwordController.text.isNotEmpty &&
+                                    _confirmController.text.isNotEmpty) {
+                                  if (_passwordController.text ==
+                                      _confirmController.text) {
+                                    authControllars.createaccount(
+                                        _emailcontroller.text,
+                                        _passwordController.text);
+                                    Get.offAll(Bottomnavbar());
+                                  } else {
+                                    Get.snackbar('Passwords do not match',
+                                        'for help go to help section');
+                                  }
+                                } else {
+                                  Get.snackbar(
+                                    'Please fill all fields',
+                                    "why not fill all fields",
+                                  );
+                                }
+                              },
+                              size: allsize.large,
+                              text: "Register",
+                              color: allcolors.primarycolor,
+                              Textcolor: allcolors.white),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Center(
+                  child: InkWell(
+                    onTap: () {},
+                    child: Text(
+                      'Already have an account',
+                      textAlign: TextAlign.end,
+                      style: allstyles.subtitlestyle,
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
-
-    );
+        ),
+      );
+    });
   }
 }
--- lib/screen/profile.dart
+++ lib/screen/profile.dart
@@ -1,10 +1,51 @@
+import 'package:ecommerce_with_firebase/screen/login.dart';
+import 'package:ecommerce_with_firebase/utility/all_colors.dart';
+import 'package:ecommerce_with_firebase/utility/all_styles.dart';
+import 'package:ecommerce_with_firebase/widgets/custom_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
 
 class Profile extends StatelessWidget {
-  const Profile({super.key});
-
+  Profile({super.key});
+  FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    User? user = _firebaseAuth.currentUser;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              'Name: Hridoy khan',
+              style: allstyles.headingstyle,
+            ),
+            Text('Age: 23'),
+            Text(
+              'Email:${user?.email}',
+              style: allstyles.subtitlestyle,
+            ),
+            SizedBox(height: 50),
+            Center(
+              child: Custombutton(
+                  height: 50,
+                  width: 100,
+                  ontap: () {
+                    _firebaseAuth.signOut();
+                    Get.offAll(login());
+                  },
+                  size: 14,
+                  text: 'Log Out',
+                  color: Colors.red,
+                  Textcolor: allcolors.white),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
